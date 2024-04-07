8ef05f4af4ef1f36e0a067139f6af08ce3b4d6b2
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2024-04-07 20:55:30 +0530

    implement display

--- AdjacencyMatrix.h
+++ AdjacencyMatrix.h
@@ -3,28 +3,38 @@
 #include <iostream>
 #include <unordered_map>
 
-/*
-    User can overwrite U to create a weighted graphs
-*/
-
 template <class T>
 class Graph
 {
 public:
+    Graph() : adjMatrix() {};
     void AddEdge(T FirstVertex, T SecondVertex, bool direction = false);
+    void display();
 private:
     std::unordered_map<T, std::unordered_map<T, bool>> adjMatrix;    
 };
 
 template <class T>
-void Graph<T>::AddEdge(T FirstVertex, T SecondVertex, bool direction = false)
+void Graph<T>::AddEdge(T FirstVertex, T SecondVertex, bool direction)
 {
-    /*
-        WeightLR acts an default weight from First to Second
-    */
     adjMatrix[FirstVertex][SecondVertex] = true;
 
     // default its undirected
     if(!direction)
         adjMatrix[SecondVertex][FirstVertex] = true;
+}
+
+template <class T>
+void Graph<T>::display()
+{
+    for(const auto &i : adjMatrix)
+    {
+        std::cout << i.first << " ---> ";
+        for(const auto &j : i.second) 
+        {
+            if(j.second)
+                std::cout << j.first << " ";
+        }
+        std::cout << '\n';
+    }
 }
