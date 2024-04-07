8cf17cc18438eee5c06ecc83c7b2f3b93a04e6a7
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2024-04-07 21:35:58 +0530

    implement BFS

--- AdjacencyList.h
+++ AdjacencyList.h
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <unordered_map>
 #include <list>
+#include <vector>
+#include <queue>
 
 template <class T>
 class Graph
@@ -11,9 +13,9 @@ public:
     Graph() = default;
     void AddEdge(T u, T v, bool direction = false);
     void display();
-    void DisplayVectoredList();
+    std::vector<T> BreadthFirstSearch(T SourceNode);
 private:
-    std::unordered_map<T, std::list<T>> adj{};
+    std::unordered_map<T, std::list<T>> adjList{};
 
     /*
         std::vector<std::vector<T>>
@@ -26,25 +28,57 @@ template <class T>
 void Graph<T>::AddEdge(T u, T v, bool direction)
 {
     //create an edge from u to v
-    adj[u].push_back(v);
+    adjList[u].push_back(v);
 
     // if undirected graph
     if(!direction)
     {
-        adj[v].push_back(u);
+        adjList[v].push_back(u);
     }
 }
 
 template <class T>
 void Graph<T>::display()
 {
-    for(auto i : adj)
+    for(const auto &i : adjList)
     {
         std::cout << i.first << " ---> ";
-        for(auto j: i.second)
+        for(const auto &j: i.second)
         {
             std::cout << j << ", ";
         }
         std::cout << '\n';
     }
+}
+
+template <class T>
+std::vector<T> Graph<T>::BreadthFirstSearch(T SourceNode)
+{
+    std::queue<T> q;
+    std::unordered_map<T, bool> visited;
+    std::vector<T> ans;
+
+    q.push(SourceNode);
+    visited[SourceNode] = true;
+
+    while(!q.empty())
+    {
+        T FrontNode = q.front();
+        q.pop();
+        
+        // store into answer
+        ans.push_back(FrontNode);
+
+        // traverse all neighbours of FrontNode;
+        for(const auto &i : adjList[FrontNode])
+        {
+            if(!visited[i])
+            {
+                q.push(i);
+                visited[i] = true;
+            }
+        }
+    }
+
+    return ans;
 }
